bb94ab649b9ec5b67cde8e67cdf62f3a4785e69d
Chaofeng Chen <chaofeng.chen@gdit.com>
2017-02-26 17:55:40 +0000

    fix variable interpret in setup_pipe.sh

--- pipeline_template.sh
+++ pipeline_template.sh
@@ -22,31 +22,31 @@ read -p 'Base working directory: ' base_dir
 
 echo "create pipeline setup script..."
 
-cat > ${base_dir}/setup_pipe.sh <<SETUP_PIPE
+cat > $(eval echo ${base_dir})/setup_pipe.sh <<SETUP_PIPE
 #!/bin/bash
 
 # export env variables
-[ -z ${GIT_USER} ] || (echo "GIT_USER and GIT_TOKEN not set" && exit -1)
-[ -z ${GIT_TOKEN} ] || (echo GIT_TOKEN not set" && exit -1)
+[ -z \${GIT_USER} ] || (echo "GIT_USER and GIT_TOKEN not set" && exit -1)
+[ -z \${GIT_TOKEN} ] || (echo GIT_TOKEN not set" && exit -1)
 
 CONCOURSE_IP=`hostname -I | cut -d' ' -f1`
-[[ ${CONCOURSE_IP} ]] || CONCOURSE_IP=127.0.0.1
+[[ \${CONCOURSE_IP} ]] || CONCOURSE_IP=127.0.0.1
 
-./fly --target ${project} login -c http://${CONCOURSE_IP}:8080
+./fly --target ${project} login -c http://\${CONCOURSE_IP}:8080
 
 # setup all dev ci pipelines
 
-for pipe in $(ls -1 *.pipe); do
-    pipe_name=$(echo ${pipe} |cut -f1 -d'.')
-    ./fly -t ${project} set-pipeline -p ${pipe_name} -c ${pipe} --var git_user=${GIT_USER} --var git_token=${GIT_TOKEN}
+for pipe in \$(ls -1 *.pipe); do
+    pipe_name=\$(echo \${pipe} |cut -f1 -d'.')
+    ./fly -t ${project} set-pipeline -p \${pipe_name} -c \${pipe} --var git_user=\${GIT_USER} --var git_token=\${GIT_TOKEN}
 
     # start all pipelines
-    ./fly -t ${project} unpause-pipeline --pipeline ${pipe_name}
+    ./fly -t ${project} unpause-pipeline --pipeline \${pipe_name}
 done
 SETUP_PIPE
 
-chmod +x ${base_dir}/setup_pipe.sh
+chmod +x $(eval echo ${base_dir})/setup_pipe.sh
 
 echo "create pipeline directory structure..."
-[ -d ${base_dir}/tasks ] || mkdir ${base_dir}/tasks
-[ -d ${base_dir}/scripts ] || mkdir ${base_dir}/scripts
+[ -d $(eval echo ${base_dir})/tasks ] || mkdir $(eval echo ${base_dir})/tasks
+[ -d $(eval echo ${base_dir})/scripts ] || mkdir $(eval echo ${base_dir})/scripts
