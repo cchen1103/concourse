3aa821c5613719b5790484ab72b6e68fd0d0d38d
Chaofeng Chen <chaofeng.chen@gdit.com>
2017-03-14 14:36:21 +0000

    simplify to ubuntu only due to requie kernel version > 3.19

--- install_concourse.sh
+++ install_concourse.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 
-set -x
+set -uexo pipefail
 
 # define concourse web url ip
+
+compose_ver=1.11.2
 web_url=127.0.0.1
-[[ $1 ]] && web_url=$1
+concourse_path='~/concourse'
 
-# verify docker is installed
-if [ -n "$(command -v yum)" ]; then
-    yum -y update
-    yum -y install docker-engine
-elif [ -n "$(command -v apt-get)" ]; then
-    curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
-    add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
-    apt-get update
-    apt-get -y install docker-engine
-fi
-
-[ -d ~/concourse ] || mkdir ~/concourse
-
-curl -L "https://github.com/docker/compose/releases/download/1.10.1/docker-compose-$(uname -s)-$(uname -m)" \
-   -o ~/concourse/docker-compose && chmod +x ~/concourse/docker-compose
-
+[ -d ${concourse_path} ] || mkdir ${concourse_path}
 export COMPOSE_API_VERSION=1.18
-export PATH=$PATH:~/concourse
+export PATH=$PATH:${concourse_path}
 export CONCOURSE_EXTERNAL_URL=http://${web_url}:8080
 
+# install docker engine. Although both CentOS and Debian OS installation are provide,
+# concourse in docker requires kernel version > 3.19.
+
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+apt-get update && apt-get -y install docker-engine
+
+curl -L "https://github.com/docker/compose/releases/download/${compose_ver}/docker-compose-$(uname -s)-$(uname -m)" \
+   -o ${concourse_path}/docker-compose && chmod +x ${concourse_path}/docker-compose
+
 # setup keys for concourse
-cd ~/concourse
+
+cd ${concourse_path}
 mkdir -p keys/web keys/worker
 rm -rf keys/web/* keys/worker/*
 
@@ -38,11 +36,10 @@ ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
 cp keys/worker/worker_key.pub keys/web/authorized_worker_keys
 cp keys/web/tsa_host_key.pub keys/worker
 
-if [ -n "$(command -v systemctl)" ]; then
-    systemctl enable docker
-    systemctl start docker
-elif [ -n "$(command -v service)" ]; then
-    service docker start
-fi
+# start docker service
+
+service docker start
+
+# launch concourse in docker
 
 docker-compose up > /dev/null &
